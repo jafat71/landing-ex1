82d42deb8b94a72ed6222786039212a37c3c04d1
jzafvt771 <ijmm7171@gmail.com>
2024-04-25 05:37:24 -0500

    init formik

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,68 +1,23 @@
-import { useState } from "react";
+import { useFormik } from "formik";
 import ButtonPrimary from "./ButtonPrimary";
 import gsap from "gsap"
 import { useEffect,useRef } from 'react'
 
 const ContactForm = () => {
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        theme: "",
-        country: "",
-        doubt: ""
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            theme: "",
+            country: "",
+            doubt:"",
+        },
+        onSubmit: data => {
+            console.log(data)
+        }
     });
-    const [formErrors, setFormErrors] = useState({});
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const errors = validateForm(formData);
-        setFormErrors(errors);
-
-        if (Object.keys(errors).length === 0) {
-            console.log("Form data:", formData);
-        }
-    };
-
-    const validateForm = (data) => {
-        let errors = {};
-
-        // Validación del nombre
-        if (!data.name.trim()) {
-            errors.name = "Name is required";
-        }
-
-        // Validación del correo electrónico
-        if (!data.email.trim()) {
-            errors.email = "Email is required";
-        } else if (!/\S+@\S+\.\S+/.test(data.email)) {
-            errors.email = "Invalid email address";
-        }
-
-        // Validación del tema
-        if (!data.theme.trim()) {
-            errors.theme = "Theme is required";
-        }
-
-        // Validación del país
-        if (!data.country.trim()) {
-            errors.country = "Country is required";
-        }
-
-        // Validación de la duda
-        if (!data.doubt.trim()) {
-            errors.doubt = "Doubt is required";
-        }
-
-        return errors;
-    };
+    
 
     const imageRef = useRef(null);
     
@@ -102,49 +57,44 @@ const ContactForm = () => {
 
             <div className="flex flex-col items-center justify-center">
                 <h1 className="font-extrabold text-7xl my-4 mx-2 bg-gradient-to-r text-transparent bg-clip-text from-bgGradient-start-500 to-bgGradient-end-500 dark:from-bgOrange dark:to-bgRose transition-all duration-300">Still have doubts?</h1>
-                <form className="bg-white dark:bg-bgBlack dark:text-white px-20 py-14 rounded-md shadow-sm shadow-black flex flex-col gap-4" onSubmit={handleSubmit}>
+                <form noValidate onSubmit={formik.handleSubmit} className="bg-white dark:bg-bgBlack dark:text-white px-20 py-14 rounded-md shadow-sm shadow-black flex flex-col gap-4">
                     <h2 className="text-bgBlack dark:text-white text-2xl font-semibold opacity-70">Contact us</h2>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black ${formErrors.name ? "border-red-500" : ""}`}
+                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black`}
                             type="text"
                             name="name"
                             placeholder="Name"
-                            value={formData.name}
-                            onChange={handleChange}
+                            onChange={formik.handleChange}
+                            value = {formik.values.name}
                         />
-                        {formErrors.name && <span className="text-red-500">{formErrors.name}</span>}
-                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black ${formErrors.email ? "border-red-500" : ""}`}
+                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black`}
                             type="email"
                             name="email"
                             placeholder="Email"
-                            value={formData.email}
-                            onChange={handleChange}
+                            onChange={formik.handleChange}
+                            value = {formik.values.email}
                         />
-                        {formErrors.email && <span className="text-red-500">{formErrors.email}</span>}
-                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black ${formErrors.theme ? "border-red-500" : ""}`}
+                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black`}
                             type="text"
                             name="theme"
                             placeholder="Theme"
-                            value={formData.theme}
-                            onChange={handleChange}
+                            onChange={formik.handleChange}
+                            value = {formik.values.theme}
                         />
-                        {formErrors.theme && <span className="text-red-500">{formErrors.theme}</span>}
-                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black ${formErrors.country ? "border-red-500" : ""}`}
+                        <input className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black`}
                             type="text"
                             name="country"
                             placeholder="Country"
-                            value={formData.country}
-                            onChange={handleChange}
+                            onChange={formik.handleChange}
+                            value = {formik.values.country}
                         />
-                        {formErrors.country && <span className="text-red-500">{formErrors.country}</span>}
-                        <textarea className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black md:col-span-2 resize-none ${formErrors.doubt ? "border-red-500" : ""}`}
+                        <textarea className={`p-4 rounded-md shadow-sm outline-none bg-inherit text-bgBlack dark:text-white shadow-black md:col-span-2 resize-none`}
                             name="doubt"
                             placeholder="Your doubt is...."
-                            value={formData.doubt}
-                            onChange={handleChange}
+                            onChange={formik.handleChange}
+                            value = {formik.values.doubt}
                         />
-                        {formErrors.doubt && <span className="text-red-500">{formErrors.doubt}</span>}
                     </div>
                     <ButtonPrimary type="submit" text={"Get Started"} />
                 </form>
